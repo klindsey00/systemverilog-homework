2dd0f4ece4b208417b30c6aa9e555234f8bbd21d
klindsey00 <kadenlindsey0033@gmail.com>
2026-07-28 12:57:55 -0400

    Update 01_07_mux_using_narrow_data_muxes.sv

--- 01_combinational_logic/01_07_mux_using_narrow_data_muxes/01_07_mux_using_narrow_data_muxes.sv
+++ 01_combinational_logic/01_07_mux_using_narrow_data_muxes/01_07_mux_using_narrow_data_muxes.sv
@@ -29,5 +29,20 @@ module mux_4_1
   // Implement mux_4_1 with 4-bit data
   // using two instances of mux_4_1_width_2 with 2-bit data
 
-
+  mux_lower mux_4_1_width_2(
+    .d0  (d0[1:0]),
+    .d1  (d1[1:0]),
+    .d2  (d2[1:0]),
+    .d3  (d3[1:0]),
+    .sel (sel[1:0]),
+    .y   (y[1:0])
+  );
+  mux_upper mux_4_1_width_2(
+    .d0  (d0[3:2]),
+    .d1  (d1[3:2]),
+    .d2  (d2[3:2]),
+    .d3  (d3[3:2]),
+    .sel (sel[1:0]),
+    .y   (y[3:2])
+  );
 endmodule
