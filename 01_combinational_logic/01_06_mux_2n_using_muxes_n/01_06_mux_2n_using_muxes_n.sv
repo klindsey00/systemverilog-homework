70f9cb08fea23f9d9d5595171a395a74d41352b2
klindsey00 <kadenlindsey0033@gmail.com>
2026-07-28 12:53:38 -0400

    Update 01_06_mux_2n_using_muxes_n.sv

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -26,6 +26,29 @@ module mux_4_1
 
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
+  logic [3:0] y_mux0, y_mux1;
+
+  mux0 mux_2_1(
+    .d0  (d0),
+    .d1  (d1),
+    .sel (sel[0]),
+    .y   (y_mux0)
+  );
+
+  mux1 mux_2_1(
+    .d0  (d2),
+    .d1  (d3),
+    .sel (sel[0]),
+    .y   (y_mux1)
+  );
+
+  mux3 mux_2_1(
+    .d0  (y_mux0),
+    .d1  (y_mux1),
+    .sel (sel[1]),
+    .y   (y)
+  );
+  
 
 
 endmodule
